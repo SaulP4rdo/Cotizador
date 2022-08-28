dba2cbbd6826a71c226af377784b0ad85e244492
Saul <saul_p4rdo@icloud.com>
2022-08-28 05:35:51 -0500

    Creando El Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import Appseguro from "./components/Appseguro";
 function App() {
-  return <h1>App</h1>;
+  return <Appseguro />;
 }
 
 export default App;
---
+++ src/components/Appseguro.jsx
@@ -0,0 +1,19 @@
+import Formulario from "./Formulario";
+
+const Appseguro = () => {
+  return (
+    <>
+      <header className="m-10">
+        <h1 className="text-white text-center text-4xl font-black">
+          Cotizador de seguros de auto
+        </h1>
+      </header>
+
+      <main className="bg-white md:w-2/3 lg:w1/2 mx-auto shadow rounded-lg p-10">
+        <Formulario />
+      </main>
+    </>
+  );
+};
+
+export default Appseguro;
